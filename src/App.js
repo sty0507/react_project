f6b5b024741b0cd49d32e4da0435a85f8f2aa263
sty0507 <styoung0507@gmail.com>
2022-06-30 17:07:48 +0900

    kakao

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import Chating from "./components/chating";
 import Chatting from "./components/Chatting";
 import Friend from "./components/Friend";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Chatting />} />
           <Route path="/Friend" element={<Friend />} />
           <Route path="/chat" element={<Chating />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
