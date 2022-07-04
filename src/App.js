5a0617369698de2ca58c0f85aa550003c5305914
sty0507 <styoung0507@gmail.com>
2022-07-04 09:02:00 +0900

    kakao8

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React from "react";
 import Chating from "./components/chating";
 import Chatting from "./components/Chatting";
 import Friend from "./components/Friend";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Chatting />} />
-          <Route path="/Friend" element={<Friend />} />
-          <Route path="/chat" element={<Chating />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Chatting />} />
+        <Route path="/Friend" element={<Friend />} />
+        <Route path="/chat" element={<Chating />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Chatting />} />
+//           <Route path="/Friend" element={<Friend />} />
+//           <Route path="/chat" element={<Chating />} />
+//         </Routes>
+//       </BrowserRouter>
+//     );
+//   }
+// }
 
 // const App = () => {
 //   return (
